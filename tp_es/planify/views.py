8421a1694406cf057f7b99673f074b2a47f91fa8
MEduardaNunes <mecosta961@gmail.com>
2026-03-31 16:06:12 -0300

    add pagina inicial do calendario e do usuário

--- tp_es/planify/views.py
+++ tp_es/planify/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def calendar(request):
+    return render(request, "calendar.html")
+
+def user_space(request):
+    return render(request, "user.html")
--- tp_es/tp_es/urls.py
+++ tp_es/tp_es/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from planify.views import home
+from planify.views import home, calendar, user_space
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('calendar/', calendar),
+    path('user/', user_space),
 ]
