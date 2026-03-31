633bc71357812359acf2eca1cd26712ac9016031
MEduardaNunes <mecosta961@gmail.com>
2026-03-31 15:19:08 -0300

    fix name unico de user

--- tp_es/planify/models.py
+++ tp_es/planify/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
 
     def __str__(self):
