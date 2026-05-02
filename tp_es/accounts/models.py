0d66007e4e255ba0bebebeb10328ea3790013528
MEduardaNunes <mecosta961@gmail.com>
2026-05-02 16:00:11 -0300

    fix cores default

--- tp_es/accounts/models.py
+++ tp_es/accounts/models.py
@@ -19,12 +19,12 @@ class UserThemePreference(models.Model):
 
 	base_clr = models.CharField(max_length=7, default="#f8f9ff", validators=[hex_color_validator])
 	line_clr = models.CharField(max_length=7, default="#42434a", validators=[hex_color_validator])
-	hover_clr = models.CharField(max_length=7, default="#eef2ff", validators=[hex_color_validator])
+	hover_clr = models.CharField(max_length=7, default="#e2d6ff", validators=[hex_color_validator])
 	text_clr = models.CharField(max_length=7, default="#ffffff", validators=[hex_color_validator])
-	accent_clr = models.CharField(max_length=7, default="#59e7ec", validators=[hex_color_validator])
+	accent_clr = models.CharField(max_length=7, default="#6344ab", validators=[hex_color_validator])
 	secondary_text_clr = models.CharField(max_length=7, default="#111827", validators=[hex_color_validator])
-	container_background_base = models.CharField(max_length=7, default="#f3f0fe", validators=[hex_color_validator])
-	secondary_base_clr = models.CharField(max_length=7, default="#ce9aff", validators=[hex_color_validator])
+	container_background_base = models.CharField(max_length=7, default="#ecf4fe", validators=[hex_color_validator])
+	secondary_base_clr = models.CharField(max_length=7, default="#60a5fa", validators=[hex_color_validator])
 	sidebar_gradient_start = models.CharField(max_length=7, default="#8b5cf6", validators=[hex_color_validator])
 	sidebar_gradient_end = models.CharField(max_length=7, default="#60a5fa", validators=[hex_color_validator])
 
