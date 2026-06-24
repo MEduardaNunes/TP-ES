9f3eefb2bb5c082d1b173add35047bfb79c9cf86
beacamx <beatrizcgp1@gmail.com>
2026-06-24 11:22:08 -0300

    corrigindo CT02 schedules_edit

--- cypress/e2e/schedules_edit.cy.js
+++ cypress/e2e/schedules_edit.cy.js
@@ -90,7 +90,10 @@ it('CT01: edita nome de agenda e verifica alteração', () => {
     cy.reload();
     cy.switchTab('tab-eventos');
 
-    cy.contains(newEventTitle).should('be.visible');
-    cy.contains(eventTitle).should('not.exist');
+    cy.get('[data-cy="container-eventos"]')
+      .contains(newEventTitle).should('be.visible');
+
+    cy.get('[data-cy="container-eventos"]')
+      .contains(eventTitle).should('not.exist');
   });
 });
